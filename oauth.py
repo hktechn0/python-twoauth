2c694bae28be1ba011d68ac3eebc6ea923948d10
Hirotaka Kawata <hktechno@hotmail.com>
2009-11-26 02:49:28 +0900

    fix generate signature

--- oauth.py
+++ oauth.py
@@ -3,21 +3,32 @@ import urllib, urllib2
 import hmac, hashlib
 import cgi
 
-# Consumer Key
-ckey = "3vqTk4hvNAno7NGzgBpdg"
-# Consumer Secret
-csecret = "PaeZx823Laxz3L5OF1R32zk9klW92tVyqn5mXjAoIw"
+#
+# Twitter OAuth Sample Script
+# * techno - Hirotaka Kawata
+# * http://techno-st.net/
+#
 
-def make_signature(params, url, csecret, secret = ""):
+# Consumer Key
+ckey = ""
+# Consumer Secret
+csecret = ""
+
+# Access Token
+atoken = ""
+# Access Token Secret
+atoken_secret = ""
+
+def make_signature(params, url, method, csecret, secret = ""):
     # Generate Signature Base String
     plist = []
     for i in sorted(params):
         plist.append("%s=%s" % (i, params[i]))
 
     pstr = "&".join(plist)
-    msg = "GET&%s&%s" % (
-        urllib.quote(url, ""), urllib.quote(pstr, ""))
-
+    msg = "%s&%s&%s" % (method, urllib.quote(url, ""), 
+                        urllib.quote(pstr, ""))
+   
     # Calculate Signature
     h = hmac.new("%s&%s" % (csecret, secret), msg, hashlib.sha1)
     sig = h.digest().encode("base64").strip()
@@ -35,6 +46,13 @@ def init_params():
 
     return p
 
+def oauth_header(params):
+    plist = []
+    for p in params:
+        plist.append('%s="%s"' % (p, urllib.quote(params[p])))
+        
+    return "OAuth %s" % (", ".join(plist))
+
 # Request Token URL
 reqt_url = 'http://twitter.com/oauth/request_token'
 # Authorize URL
@@ -43,81 +61,103 @@ auth_url = 'http://twitter.com/oauth/authorize'
 acct_url = 'http://twitter.com/oauth/access_token'
 # status update
 post_url = 'http://twitter.com/statuses/update.xml'
-# acount verify_credentials
-avfy_url = 'http://twitter.com/account/verify_credentials.xml'
 # show friends timeline
 frtl_url = 'http://twitter.com/statuses/friends_timeline.xml'
 
-# Request Parameters
-params = init_params()
+if not atoken and not atoken_secret:
+    # Request Parameters
+    params = init_params()
 
-print "Get request token:",
+    print "Get request token:",
+    
+    # Generate Signature
+    sig = make_signature(params, reqt_url, "GET", csecret)
+    params["oauth_signature"] = sig
 
-# Generate Signature
-sig = make_signature(params, reqt_url, csecret)
-params["oauth_signature"] = sig
+    # Get Token
+    req = urllib2.Request("%s?%s" % (reqt_url, urllib.urlencode(params)))
+    resp = urllib2.urlopen(req)
+    
+    print "\t[OK]"
+    
+    # Parse Token Parameters
+    ret = cgi.parse_qs(resp.read())
+    token = ret["oauth_token"][0]
+    token_secret = ret["oauth_token_secret"][0]
 
-# Get Token
-req = urllib2.Request("%s?%s" % (reqt_url, urllib.urlencode(params)))
-resp = urllib2.urlopen(req)
+    # Get PIN
+    print "* Please access to this URL, and allow."
+    print "> %s?%s=%s" % (auth_url, "oauth_token", token)
+    print "\n* After that, will display 7 digit PIN, input here."
+    print "PIN ->",
+    pin = raw_input()
+    pin = int(pin)
+    
+    print "Get access token:",
+    
+    # Generate Access Token Request
+    params = init_params()
+    params["oauth_verifier"] = pin
+    params["oauth_token"] = token
+    
+    sig = make_signature(params, acct_url, "GET", csecret, token_secret)
+    params["oauth_signature"] = sig
+    
+    # Get Access Token
+    req = urllib2.Request("%s?%s" % (acct_url, urllib.urlencode(params)))
+    resp = urllib2.urlopen(req)
+    
+    print "\t[OK]"
+    
+    # Parse Access Token
+    fin = cgi.parse_qs(resp.read())
+    atoken = fin["oauth_token"][0]
+    atoken_secret = fin["oauth_token_secret"][0]
+    
+    print "Access Token: %s" % atoken
+    print "Access Token Secret: %s" % atoken_secret
+    
+    print "Your screen_name is '%s'." % fin["screen_name"][0]
+    
 
-print "\t[OK]"
-
-# Parse Token Parameters
-ret = cgi.parse_qs(resp.read())
-token = ret["oauth_token"][0]
-token_secret = ret["oauth_token_secret"][0]
-
-print "* Please access to this URL, and allow."
-print "> %s?%s=%s" % (auth_url, "oauth_token", token)
-print "\n* After that, will display 7 digit PIN, input here."
-print "PIN ->",
-pin = raw_input()
-pin = int(pin)
-
-print "Get access token:",
-
-params = init_params()
-params["oauth_verifier"] = pin
-params["oauth_token"] = token
-
-sig = make_signature(params, acct_url, csecret, token_secret)
-params["oauth_signature"] = sig
-
-req = urllib2.Request("%s?%s" % (acct_url, urllib.urlencode(params)))
-resp = urllib2.urlopen(req)
-
-print "\t[OK]"
-
-fin = cgi.parse_qs(resp.read())
-atoken = fin["oauth_token"][0]
-atoken_secret = fin["oauth_token_secret"][0]
-
-print "Your screen_name is '%s'." % fin["screen_name"][0]
-
+# Update Status by OAuth Authorization
 print "What are you doing?:",
 post = raw_input()
 
 params = init_params()
 params["oauth_token"] = atoken
-params["status"] = post.decode("utf-8")
+params["status"] = urllib.quote(post)
 
-sig = make_signature(params, post_url, csecret, atoken_secret)
+sig = make_signature(params, post_url, "POST", csecret, atoken_secret)
 params["oauth_signature"] = sig
 
+del params["status"]
+
 req = urllib2.Request(post_url)
-req.add_data(urllib.urlencode(params))
+req.add_data("status=%s" % urllib.quote(post))
+req.add_header("Authorization", oauth_header(params))
 
-print urllib.urlencode(params)
+try:
+    resp = urllib2.urlopen(req)
+except urllib2.HTTPError, e:
+    print "Error: %s" % e
+    print e.read()
 
-urllib.urlencode(params)
-#req = urllib2.Request("%s?%s" % (frtl_url, urllib.urlencode(params)))
-req.add_header("Authorization", "OAuth")
+# Get Friends Timeline by OAuth Authorization
+params = init_params()
+params["oauth_token"] = atoken
+
+sig = make_signature(params, frtl_url, "GET", csecret, atoken_secret)
+params["oauth_signature"] = sig
+
+req = urllib2.Request(frtl_url)
+req.add_header("Authorization", oauth_header(params))
 resp = urllib2.urlopen(req)
 
-#import test
-#tl = test.tw_xmlparse(resp.read())
-#for post in tl.statuses:
-#    print "@%s\t%s" % (post["user"]["screen_name"], post["text"])
+# Show Timeline
+import xml_parse
+tl = xml_parse.tw_xmlparse(resp.read())
+for post in tl.statuses:
+    print "@%s\t%s" % (post["user"]["screen_name"], post["text"])
 
 print "Done!!"
